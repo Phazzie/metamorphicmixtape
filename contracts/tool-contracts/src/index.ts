7c8b28c0f20b0bf4d782c81861aa3673baf99e8b
Phazzie <sailorbeefalo@gmail.com>
2025-10-31 16:39:01 -0400

    feat: add shared contract workspace and tests

---
+++ contracts/tool-contracts/src/index.ts
@@ -0,0 +1,6 @@
+export * from './contract.js';
+export * from './songwriting.js';
+export * from './analysis.js';
+export * from './meta.js';
+export * from './suno.js';
+export * from './collaboration.js';
